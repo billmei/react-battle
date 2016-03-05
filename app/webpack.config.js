0cffb269b8082d97f0e27c9ad5bddefe12be312b
Bill Mei <billmei@users.noreply.github.com>
2016-03-05 12:40:56 -0800

    Finish configuring webpack

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,3 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
 module.exports = {
   entry: [
     './app/index.js',
@@ -8,7 +15,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader',},
+      {test: /\.js$/, include: __dirname + '/app', loader: 'babel-loader',},
     ],
   },
+  plugin: [HtmlWebpackPluginConfig],
 }
