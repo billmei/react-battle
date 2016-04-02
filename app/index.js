f059fb4850dfe903afd1e8a4bf20fea1d9327c56
Bill Mei <billmei@users.noreply.github.com>
2016-04-02 14:31:36 -0700

    Link class

--- app/index.js
+++ app/index.js
@@ -7,6 +7,23 @@ var USER_DATA = {
   image: 'https://avatars0.githubusercontent.com/u/2933420?v=3&s=460',
 }
 
+var Link = React.createClass({
+  changeURL: function() {
+    window.location.replace(this.props.href);
+  },
+
+  render: function() {
+    return (
+      <span
+        style={{color: 'blue', cursor: 'pointer'}}
+        onClick={this.changeURL}
+      >
+        {this.props.children}
+      </span>
+    )
+  }
+})
+
 var ProfilePic = React.createClass({
   render: function() {
     return (
@@ -19,9 +36,9 @@ var ProfileLink = React.createClass({
   render: function() {
     return (
       <div>
-        <a href={'https://www.github.com/' + this.props.username }>
+        <Link href={'https://www.github.com/' + this.props.username }>
           {this.props.username}
-        </a>
+        </Link>
       </div>
     )
   }
