9523e6168fc6e7d34b01c0f655e470634be699d4
Bill Mei <billmei@users.noreply.github.com>
2016-04-10 15:45:56 -0700

    Refactor

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,10 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
-var Link = require('react-router').Link;
-var UserDetails = require('./UserDetails');
+var ReactRouter = require('react-router')
+var Link = ReactRouter.Link;
 var UserDetailsWrapper = require('./UserDetailsWrapper')
+var UserDetails = require('./UserDetails');
 
 function ConfirmBattle (props) {
   return props.isLoading === true ? (
@@ -14,31 +15,33 @@ function ConfirmBattle (props) {
       </div>
     </div>
   ) : (
-    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
-      <h1>Confirm Players</h1>
-      <div className='col-sm-10 col-sm-offset-1'>
-        <div className="row">
-          <UserDetailsWrapper header={'Player One'}>
-            <UserDetails info={props.playersInfo[0]} />
-          </UserDetailsWrapper>
-          <UserDetailsWrapper header={'Player Two'}>
-            <UserDetails info={props.playersInfo[1]} />
-          </UserDetailsWrapper>
-        </div>
-      </div>
-      <div className='col-sm-1- col-sm-offset-1'>
-        <div className="row">
-          <div className='col-xs-12' style={styles.space}>
-            <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
-              Initiate Battle!
-            </button>
+    <div className="jumbotron text-center" style={styles.transparentBg}>
+      <div className="row">
+        <div className='col-sm-10 col-sm-offset-1'>
+          <h1>Confirm Players</h1>
+          <div className="row">
+            <UserDetailsWrapper header={'Player One'}>
+              <UserDetails info={props.playersInfo[0]} />
+            </UserDetailsWrapper>
+            <UserDetailsWrapper header={'Player Two'}>
+              <UserDetails info={props.playersInfo[1]} />
+            </UserDetailsWrapper>
           </div>
-          <div className='col-xs-12' style={styles.space}>
-            <Link to="/playerOne">
-              <button className="btn btn-lg btn-danger">
-                Reselect Players
+        </div>
+        <div className='col-sm-1- col-sm-offset-1'>
+          <div className="row">
+            <div className='col-xs-12' style={styles.space}>
+              <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+                Initiate Battle!
               </button>
-            </Link>
+            </div>
+            <div className='col-xs-12' style={styles.space}>
+              <Link to="/playerOne">
+                <button className="btn btn-lg btn-danger">
+                  Reselect Players
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- app/config/routes.js
+++ app/config/routes.js
@@ -2,8 +2,8 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
+var IndexRoute = ReactRouter.IndexRoute;
 
 var Main = require('../components/Main');
 var Home = require('../components/Home');
