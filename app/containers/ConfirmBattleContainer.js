d31b060300a6e2967db67a6aa8d76628957b6795
Bill Mei <billmei@users.noreply.github.com>
2016-04-09 18:44:59 -0700

    Github api requests

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
-var ConfirmBattle = require('../components/ConfirmBattle')
+var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -22,8 +23,13 @@ var ConfirmBattleContainer = React.createClass({
     console.log('ConfirmBattleContainer', 'componentDidMount');
     // onShow: Make ajax requests
     var query = this.props.location.query;
-    // Fetch info from github then update state
-    // this.state.isLoading = false;
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]],
+        })
+      }.bind(this));
   },
 
   componentWillReceiveProps: function() {
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,11 +1,26 @@
 var axios = require('axios');
 
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var queryString = "?client_id=" + id + "&client_secret=" + sec;
 
-var helpers = {
-  getPlayersInfo = function(username) {
-    // fetch data from GitHub
-  },
+function getUserInfo(username) {
+  var GITHUB_URL = 'https://api.github.com/users/';
+  return axios.get(GITHUB_URL + username + queryString);
 };
 
+var helpers = {
+  getPlayersInfo: function(players) {
+    return axios.all(players.map(function(username) {
+      return getUserInfo(username);
+    })).then(function(info) {
+      return info.map(function(user) {
+        return user.data;
+      })
+    }).catch(function(err) {
+      console.warn('Error in getPlayersInfo', err);
+    });
+  },
+};
 
 module.exports = helpers;
