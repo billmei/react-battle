ea1ba1a074f20e9495cc2a2a088e62aea3602b5d
Bill Mei <billmei@users.noreply.github.com>
2016-06-18 22:04:33 -0700

    Animated loading spinner

--- app/components/LoadingSpinner.js
+++ app/components/LoadingSpinner.js
@@ -1,14 +1,39 @@
 var React = require('react');
 
-function LoadingSpinner() {
-  return (
-  	<div className="row">
-  	  <div className="col-xs-12 text-center">
-  	    <p><strong>Loading!</strong></p>
-  	    <p><span className="glyphicon glyphicon-spin glyphicon-refresh"></span></p>
-  	  </div>
-  	</div>
-  );
-};
+var LoadingSpinner = React.createClass({
+  getInitialState: function() {
+    this.originalText = "Loading";
+    return {
+      text: this.originalText,
+    }
+  },
+  componentDidMount: function() {
+    var finalText = this.originalText + '...';
+    this.interval = setInterval(function() {
+      if (this.state.text === finalText) {
+        this.setState({
+          text: this.originalText,
+        });
+      } else {
+        this.setState({
+          text: this.state.text + '.'
+        });
+      }
+    }.bind(this), 300);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+  render: function() {
+    return (
+      <div className="row">
+        <div className="col-xs-12 text-center">
+          <p><strong>{this.state.text}</strong></p>
+          <p><span className="glyphicon glyphicon-spin glyphicon-refresh"></span></p>
+        </div>
+      </div>
+    )
+  },
+});
 
 module.exports = LoadingSpinner;
